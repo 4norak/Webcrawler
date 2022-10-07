932751801b77f54fbf91c0af01cc5d31e85d81da
4norak <32487760+4norak@users.noreply.github.com>
2022-10-07 14:19:31 +0200

    Renaming and minor changes

--- default_functions.py
+++ default_functions.py
@@ -1,9 +1,9 @@
-from typing import Any
-from bs4 import Tag as _Tag
-from re import match as _match, search as _search
+from typing import Any as Any
+from bs4 import Tag as Tag
+from re import match as match, search as search
 
 
-def _getitem(tag: Tag, item: str) -> Any:
+def getitem(tag: Tag, item: str) -> Any:
     """
     Return a tag attribute by name.
 
@@ -16,32 +16,32 @@ def _getitem(tag: Tag, item: str) -> Any:
 
 # Select Functions must return bs4.Tag or List of bs4.Tag
 SELECT_FUNCTIONS = {
-    "select": _Tag.select,
-    "find": _Tag.find,
-    "find_next": _Tag.find_next,
-    "find_previous": _Tag.find_previous,
-    "find_next_sibling": _Tag.find_next_sibling,
-    "find_previous_sibling": _Tag.find_previous_sibling,
-    "find_parent": _Tag.find_parent,
-    "find_all": _Tag.find_all,
-    "find_all_next": _Tag.find_all_next,
-    "find_all_previous": _Tag.find_all_previous,
-    "find_next_siblings": _Tag.find_next_siblings,
-    "find_previous_siblings": _Tag.find_previous_siblings,
-    "find_parents": _Tag.find_parents,
+    "select": Tag.select,
+    "find": Tag.find,
+    "find_next": Tag.find_next,
+    "find_previous": Tag.find_previous,
+    "find_next_sibling": Tag.find_next_sibling,
+    "find_previous_sibling": Tag.find_previous_sibling,
+    "find_parent": Tag.find_parent,
+    "find_all": Tag.find_all,
+    "find_all_next": Tag.find_all_next,
+    "find_all_previous": Tag.find_all_previous,
+    "find_next_siblings": Tag.find_next_siblings,
+    "find_previous_siblings": Tag.find_previous_siblings,
+    "find_parents": Tag.find_parents,
     "children": (lambda tag: tag.children),
-    "getitem": _getitem,
-    "[]": _getitem
+    "getitem": getitem,
+    "[]": getitem
 }
 
 # Filter functions must return a boolean
 FILTER_FUNCTIONS = {
-    "match": (lambda tag,re: _match(re,str(tag)) is not None),
-    "match_text": (lambda tag,re: _match(re,tag.text) is not None),
-    "match_string": (lambda tag,re: _match(re,tag.string) is not None),
-    "search": (lambda tag,re: _search(re,str(tag)) is not None),
-    "search_text": (lambda tag,re: _search(re,tag.text) is not None),
-    "search_string": (lambda tag,re: _search(re,tag.string) is not None)
+    "re_match": (lambda tag,re: match(re,str(tag)) is not None),
+    "re_match_text": (lambda tag,re: match(re,tag.text) is not None),
+    "re_match_string": (lambda tag,re: match(re,tag.string) is not None),
+    "re_search": (lambda tag,re: search(re,str(tag)) is not None),
+    "re_search_text": (lambda tag,re: search(re,tag.text) is not None),
+    "re_search_string": (lambda tag,re: search(re,tag.string) is not None)
 }
 
 # Action functions should not return anything
